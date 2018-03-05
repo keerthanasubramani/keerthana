d1ac3aacc925a20c848adab7440cdf8c035a8d90
keerthanasubramani <36470183+keerthanasubramani@users.noreply.github.com>
2018-03-05 20:32:08 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class fact
+{
+public static void main(String a[])
+{
+int fact=1;
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+ fact=fact*i;
+}
+System.out.println(fact);
+}
+}
