f772b0a232b737007760c0ab77ff673a4f98a9f5
keerthanasubramani <36470183+keerthanasubramani@users.noreply.github.com>
2018-03-10 10:11:11 +0530

    Add files via upload

--- fact.java
+++ fact.java
@@ -2,15 +2,21 @@ import java.io.*;
 import java.util.*;
 class fact
 {
-public static void main(String a[])
-{
-int fact=1;
-Scanner sc=new Scanner(System.in);
-int n=sc.nextInt();
-for(int i=1;i<=n;i++)
-{
- fact=fact*i;
+    public static void main(String a[])
+    {
+        int fact=1,i;
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        for(i=1;i<=num;i++)
+       {
+            fact=fact*i;
+        }
+           System.out.println(fact);
+    }
 }
-System.out.println(fact);
-}
-}
+
+
+
+
+
+
