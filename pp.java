996dd8ebd7ee0f603ab9d23ca1dce9fad711e3dd
keerthanasubramani <36470183+keerthanasubramani@users.noreply.github.com>
2018-06-18 23:04:30 +0530

    Add files via upload

---
+++ pp.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class pp
+{
+    public static void main(String a[])
+{
+Scanner sc=new Scanner(System.in);
+double n=sc.nextInt();
+double po=sc.nextInt();
+System.out.println((int)Math.pow(n,po));}
+}
