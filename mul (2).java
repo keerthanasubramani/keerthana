ede728a3c1412442d11b4b5ce9f20943420158bb
keerthanasubramani <36470183+keerthanasubramani@users.noreply.github.com>
2018-06-27 09:41:36 +0530

    Add files via upload

---
+++ mul (2).java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class mul
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int mul=0;
+        for(int i=0;i<5;i++)
+        {
+            mul=mul+n;
+            System.out.print(mul+" ");
+        }
+    
+    }
+}
