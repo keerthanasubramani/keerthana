867010794c648c42726820100dc31e5a0b2be714
keerthanasubramani <36470183+keerthanasubramani@users.noreply.github.com>
2018-08-24 09:59:21 +0530

    Create g12.java

---
+++ g12.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class g12
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%2==0)
+        {
+            System.out.print("Even");
+        }
+        else
+        {
+            System.out.print("Odd");
+        }
+    }
+}
