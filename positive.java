ef2de4a8bc286b1d40650c35f0762597cf11e476
keerthanasubramani <36470183+keerthanasubramani@users.noreply.github.com>
2018-08-24 09:46:55 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class positive
+{
+public static void main(String ar[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a>0)
+{
+System.out.print("Positive");
+}
+else if(a==0)
+{
+System.out.print("Zero");
+}
+else if(a<0)
+{
+System.out.print("Negative");
+}
+}
+}
